ede7b80a142b936152e00cc982802b3a84333880
dchakin <chakin.2001@mail.ru>
2024-01-25 19:40:27 +0300

    upd

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main-container">
+    <text-tags :tags="tags" alignment="left" :style="{background: '#FED6BC'}"/>
+    <text-tags :tags="tags" alignment="width" :tags-font-size="20" :style="{ background: '#FFFADD'}"/>
+    <text-tags :tags="tags" alignment="left" :tags-font-size="24" :style="{width: '50%', background: '#DEF7FE'}" />
+    <text-tags :tags="tags" alignment="width" :tags-font-size="32" :style="{width: '50%', background: '#E7ECFF'}"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TextTags from "@/components/TextTags.vue";
 
 export default {
-  name: 'App',
+  data() {
+    return {
+      tags: [
+        { text: "test text 1" },
+        { text: "test text 2", icon: "mdi-home" },
+        { text: "test 3", icon: "mdi-account-heart-outline" },
+        { text: "testtext 4", icon: "mdi-airplane" },
+        { text: "testt 5", icon: "mdi-apps" },
+        { text: "t", icon: "mdi-axis" },
+      ],
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    TextTags,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped lang="scss">
+.main-container {
+  width: 100%;
+  font-size: 20px;
 }
+
 </style>
--- src/components/TextTags.vue
+++ src/components/TextTags.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="text-tags" :class="{ 'text-tags--center': alignment === 'center' }">
-    <div v-for="(tag, index) in visibleTags" :key="index" class="text-tags__tag">
-      <v-icon v-if="tag.icon">{{ tag.icon }}</v-icon>
-      {{ tag.text }}
-      <v-icon v-if="index < visibleTags.length - 1">mdi-circle-small</v-icon>
+  <div ref="refRow" class="text-tags" :class="{ 'text-tags--width': alignment === 'width' }">
+    <div v-for="(tag, index) in visibleTags" :key="index" class="text-tags__row" :style="{'fontSize': `${tagsFontSize}px`}">
+      <v-icon
+        v-if="tag === 'divider'"
+        class="text-tags__divider"
+        :style="{'font-size': `${iconSize}px`}"
+        :class="{'display-none': index === visibleTags.length - 1}"
+      >
+        mdi-circle-small
+      </v-icon>
+      
+      <div v-else class="text-tags__tag">
+        <v-icon color="#fff" :style="{'font-size': `${iconSize}px`}" v-if="tag.icon">{{ tag.icon }}</v-icon>
+        {{ tag.text }}
+      </div>
     </div>
   </div>
 </template>
@@ -18,59 +28,116 @@ export default {
     alignment: {
       type: String,
       default: 'left',
-      validator: (value) => ['left', 'center', 'width'].includes(value),
+      validator: (value) => ['left', 'width'].includes(value),
     },
+    tagsFontSize: {
+      type: Number,
+      required: false,
+      default: 16,
+    }
   },
+  
+  data() {
+    return {
+      maxWidth: 0, // максимальная ширина компонента
+      iconSize: 20, // размер иконки
+    }
+  },
+  
   computed: {
+    // отображаемые теги
     visibleTags() {
-      const maxWidth = this.alignment === 'width' ? this.calculateMaxWidth() : Infinity;
-      let currentWidth = 0;
-      let visibleTags = [];
+      let totalWidth = 0 // общая ширина всех тегов
+      let visibleTags = [] // массив отображаемых тегов
       
+      // проходимся по списку тегов
       for (const tag of this.tags) {
-        const tagWidth = this.calculateTagWidth(tag);
-        if (currentWidth + tagWidth <= maxWidth) {
-          visibleTags.push(tag);
-          currentWidth += tagWidth;
+        // для кажого тега будем считать его ширину
+        const tagWidth = this.calculateTagWidth(tag)
+        
+        // если общая ширина всех тегов меньше максимальной ширины компонента, то отображаем тег
+        if (totalWidth + tagWidth < this.maxWidth) {
+          visibleTags.push(tag) // пушим тег в массив
+          visibleTags.push('divider') // пушим разделитель
+          totalWidth += tagWidth // увеличиваем общую сумму ширины всех тегов
+          if (visibleTags.length !== 1) {
+            totalWidth += this.iconSize // увеличиваем общую сумму ширины всех тегов на ширину разделителей
+          }
         } else {
-          break;
+          break
         }
       }
       
-      return visibleTags;
+      visibleTags.pop() // удялем последний разделитель
+      return visibleTags
     },
   },
+  
+  mounted() {
+    // высчитываем максимальную ширину компонента при монтировании
+    this.calculateMaxWidth()
+    // высчитываем максимальную ширину компонента при ресайзе окна
+    window.addEventListener('resize', () => this.calculateMaxWidth())
+  },
+  
+  beforeDestroy() {
+    window.removeEventListener('resize', this.handleResize);
+  },
+  
   methods: {
+    // метод вычисления максимальной ширины компонента
     calculateMaxWidth() {
-      // Calculate the maximum width based on the parent container's width
-      const parentWidth = this.$el.parentElement.clientWidth;
-      // You can apply any logic here based on your design requirements
-      return parentWidth;
+      this.maxWidth = this.$el.clientWidth // вычисляется по ширине корневого элемента в компоненте
     },
+    
+    // метод вычисления ширины тега
     calculateTagWidth(tag) {
-      // Calculate the width of a tag based on its content and icon (if present)
-      // You can apply any logic here based on your design requirements
-      return tag.text.length * 10 + (tag.icon ? 20 : 0);
+      const tempElement = document.createElement('div') // создаем временный элемент нашего тега
+      tempElement.style.visibility = 'hidden' // скрываем
+      tempElement.style.position = 'absolute' // позиционриуем абсолютно
+      tempElement.className = 'text-tags__tag' // применяем стили
+      tempElement.style.fontSize = `${this.tagsFontSize}px` // скрываем
+      tempElement.innerHTML = tag.text // заполняем текст
+      
+      this.$el?.appendChild(tempElement) // добавляем тег на страницу
+      const tagWidth = tempElement.clientWidth // фиксируем ширину тега
+      this.$el?.removeChild(tempElement) // удаляем временный тег
+      
+      return tagWidth + (tag.icon ? this.iconSize + 4 : 0) // this.iconSize + 4px (column-gap)
     },
   },
 };
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
 .text-tags {
   display: flex;
   align-items: center;
-  flex-wrap: wrap;
+  padding: 24px 0;
   
-  &--center {
-    justify-content: center;
+  &__row {
+    display: flex;
+    align-items: center;
+  }
+  
+  &--width {
+    justify-content: space-between;
+  }
+  
+  &--left {
+    justify-content: flex-start;
   }
   
   &__tag {
     display: flex;
     align-items: center;
-    margin-right: 8px;
-    margin-bottom: 8px;
+    column-gap: 4px;
+    background: #8990b3;
+    color: #fff;
+    border-radius: 16px;
+    padding: 0 8px;
+    min-height: 24px;
+    white-space: nowrap;
   }
 }
 </style>
